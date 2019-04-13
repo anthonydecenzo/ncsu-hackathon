d6877377407d87e8cb7af2924b49b3fce02e50ca
Anthony decenzo <anthonydecenzo8@gmail.com>
2019-04-13 14:33:12 -0400

    edited for current user to add tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
 
+    @task.user_id = current_user.id
     if @task.save
       redirect_to @task
     else
