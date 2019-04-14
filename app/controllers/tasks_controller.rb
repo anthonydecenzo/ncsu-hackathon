ab41404b74e406aea9843c3a5f6238ef0576fe17
Anthony decenzo <anthonydecenzo8@gmail.com>
2019-04-14 03:00:48 -0400

    added some shit idk

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,14 +19,31 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
+
+
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+        redirect_to @task
+    else
+        render 'edit'
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
+
+
   end
 
   private
   def task_params
-    params.require(:task).permit(:name)
+    params.require(:task).permit(:name, :water)
 
   end
+
 end
